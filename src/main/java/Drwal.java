5cf395b3072d3fc0f21f355908b28038683b0260
Jakub Kisiel <kuba.kisiel@icloud.com>
2021-05-18 20:11:55 +0200

    Test commit

--- src/main/java/Drwal.java
+++ src/main/java/Drwal.java
@@ -1,28 +1,33 @@
-public class Drwal extends Thread{
+public class Drwal extends Thread
+{
     private Krolestwo k;
     private int siekiery;
     private int drewno;
+
     public Drwal(Krolestwo k, int siekiery, int drewno)
     {
-        this.k=k;
-        this.siekiery=siekiery;
-        this.drewno=drewno;
+        this.k = k;
+        this.siekiery = siekiery;
+        this.drewno = drewno;
     }
+
     @Override
     public void run()
     {
-        while(true)
+        while (true)
         {
-            synchronized(this)
+            synchronized (this)
             {
                 try
                 {
                     this.wait();
+                } catch (InterruptedException ie)
+                {
                 }
-                catch (InterruptedException ie){}
             }
             //wycinanie drewna
-            for (Toolsmith t:this.k.getToolsmith()) {
+            for (Toolsmith t : this.k.getToolsmith())
+            {
                 synchronized (t)
                 {
                     t.notify();
